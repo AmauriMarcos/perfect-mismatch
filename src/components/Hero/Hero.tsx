925670e2ae1a1b85c0c3658f52646510f71a9755
AmauriMarcos <amaurisantos.m@gmail.com>
2024-09-16 13:52:40 +0200

    merge branch

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -20,13 +20,6 @@ const Hero = () => {
             alt='Malta'
           />
         </div>
-<<<<<<< HEAD
-=======
-        
-        <div className='flex flex-col gap-4 px-4  justify-center'>
-          <h2>The answeome trip together</h2>
-          <p>{`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. `}</p>
->>>>>>> add-new-nav-layout
 
         <div className='flex flex-col gap-4 px-4 justify-center'>
           <h2>The awesome trip together</h2>
