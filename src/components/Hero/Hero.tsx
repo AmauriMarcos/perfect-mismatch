f907aa41204ba24741c87b79ccd80533b90bccf8
AmauriMarcos <amaurisantos.m@gmail.com>
2024-09-17 17:09:55 +0200

    make some adjustments

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,19 +7,19 @@ const Hero = () => {
     <div className=" w-full flex justify-between ">
       <div className="w-full flex flex-col md:flex-row justify-between items-center gap-8">
         <Image
-          className="object-cover h-[360px] min-w-[500px]"
+          className="object-cover h-[360px] min-w-0 md:min-w-[500px] "
           height={360}
           width={500}
           src="https://images.pexels.com/photos/3496763/pexels-photo-3496763.jpeg?auto=compress&cs=tinysrgb&w=600"
           alt="Malta"
         />
-        <div className="flex flex-col gap-2 items-center md:items-start">
+        <div className="flex flex-col gap-2 items-start">
           <div className="font-inter  w-full flex items-start ">
             <p className="text-slate-950 font-normal text-[.75rem]">
               Latest post
             </p>
           </div>
-          <h2 className="font-extrabold text-[2.4rem]">
+          <h2 className="font-extrabold text-[2rem] md:text-[2.4rem]">
             Eating pizza in Italy
           </h2>
           <div className="flex items-center gap-2 ">
