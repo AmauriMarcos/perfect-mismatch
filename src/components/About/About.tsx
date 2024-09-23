ed8877c7cd92820a96d0ec210d866475abd02fda
AmauriMarcos <amaurisantos.m@gmail.com>
2024-09-23 16:54:07 +0200

    Fixed about page and removed some unnecessary code

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,25 +1,30 @@
+import Image from "next/image";
 import React from "react";
-
+Image;
 
 const About = () => {
   return (
     <div className="w-full m-auto my-[5rem] mb-[10rem]">
-      <div className="w-[85%] md:w-[60%] m-auto">
-        <h2 className="text-[1.5rem] md:text-3xl font-normal font-montserrat text-center my-6">
-            Hi there, and welcome!
-        </h2>
-        <p className="font-inter text-md md:text-lg text-primary font-normal  my-[2rem]">
-          {`Hi, I'm Marija! I'm living in Serbia with my wonderful
+      <div className="w-[85%] md:w-[60%] m-auto flex flex-col-reverse md:flex-row ">
+        <div className="mr-0 md:mr-10 ">
+          <h2 className="text-[1.5rem] md:text-3xl font-normal font-montserrat text-center md:text-left my-6">
+            Hi! It's great to see you here!
+          </h2>
+          <p className="font-inter text-md md:text-lg text-primary font-normal  my-[2rem]">
+            {`Hi, I'm Marija! I'm living in Serbia with my wonderful
           family—my husband and our three energetic kids. I have a sweet tooth
           and love trying new desserts, singing, and spending time with friends
           and loved ones. Family means everything to me, and we enjoy creating
           special moments together. This space is where I share my passion for
           life, family, and all the little things that make every day special.`}
-        </p>
+          </p>
+        </div>
+        <div className="shrink-0">
+          <Image src="/MarijaAuthor.jpg" width={400} height={400} alt="" className="rounded-md" />
+        </div>
       </div>
 
       {/* You can keep the Motion component for visual interaction */}
-     
     </div>
   );
 };
